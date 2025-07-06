bcbd12840e18414f7eb2d4b9e6785cde4d193eee
ASKAT RAKHYMBEKOV <149674649+Hanbiike@users.noreply.github.com>
2025-07-06 17:49:44 +0600

    Add test selection feature and rename menu button

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ sys.stdout.reconfigure(encoding='utf-8', errors='replace')
 
 import asyncio
 from aiogram import Bot, Dispatcher
-from handlers import start, calc, profiles, parser, file_id
+from handlers import start, calc, profiles, parser, file_id, tests
 from methods import admin, users
 from keyboards import menu
 from config import BOT_TOKEN
@@ -20,6 +20,7 @@ async def main():
         users.router,
         calc.router,
         profiles.router,
+        tests.router,
         #parser.router,
         file_id.router
     )
